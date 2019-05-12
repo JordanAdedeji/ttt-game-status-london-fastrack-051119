2b99fd379557b3ae1da2c895cf28df4a2fde80d1
Jordan Adedeji <jordan_adedeji@outlook.com>
2019-05-12 12:41:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,8 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5],  # Middle row
   [6,7,8], #Bottom row
-  [0,3,6] # Left column
-  [2,5,8] # right column
-  [0,4,8] # diagonal left
+  [0,3,6], # Left column
+  [2,5,8], # right column
+  [0,4,8], # diagonal Left
   
 ]
